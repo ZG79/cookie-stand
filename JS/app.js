6eb243e23b5634e01527735cae5bc47e56ab3525
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-22 21:58:01 -0800

    complete Seattle

---
+++ JS/app.js
@@ -0,0 +1,48 @@
+'Use strict';
+
+const containerEl = document.getElementById('cookie');
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+
+
+let seattle = {
+  minNumberCustomer: 23,
+  maxNumberCustomer: 65,
+  avgCookie: 6.3,
+  cookiesSoldPerHour: [],
+  dailyTotal: 0,
+  randomNumCustomer : function () {
+    return Math.floor(Math.random()*(this.maxNumberCustomer-this.minNumberCustomer+1)+this.minNumberCustomer);
+  },
+  calculateCookie: function () {
+    for (let i=0; i<hours.length; i++){
+      let randomCustomer = this.randomNumCustomer();
+      let hourlyTotal = Math.ceil(randomCustomer * this.avgCookie); 
+      this.cookiesSoldPerHour.push(hourlyTotal);
+      this.dailyTotal+=hourlyTotal;
+    } 
+  },
+
+  render: function(){
+    //for loop to create ul-li
+    this.calculateCookie();
+    const h3Elem = document.createElement('h3');
+    h3Elem.textContent = 'Seattle location';
+    containerEl.appendChild(h3Elem);
+
+    let ulElem = document.createElement('ul');
+    for (let i=0; i<hours.length; i++){
+    let liElem = document.createElement('li');
+    liElem.textContent = `${hours[i]} ${this.cookiesSoldPerHour[i]}`;
+    ulElem.appendChild(liElem);
+    } containerEl.appendChild(ulElem);
+  },
+};
+seattle.render();
+// seattle.calculateCookie();
+// console.log(seattle.cookiesSoldPerHour);
+// console.log(seattle.dailyTotal);
+
+
+
+
